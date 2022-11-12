c496367b628ece72985f704c1e9dfb4bf02cdb98
mario <wolfbox22@gmail.com>
2022-11-11 22:03:10 -0500

    Se añadio una funcion de numero maximo

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Prueba 1");
 
+        Scanner con = new Scanner(System.in);
 
         int val1 = 10;
         System.out.println(val1);
+
+        System.out.println("Digite 2  numeros");
+        int a = con.nextInt();
+        int b = con.nextInt();
+        System.out.println(sum(a,b));
+
+
+    }
+    public static int sum(int a, int b){
+
+        int c = Math.max(a,b);
+        return c;
+
+
     }
 }
